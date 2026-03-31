ba533ae6e891d527f11d2b4f60de6636e6c85394
aidil24si <aidil24si@mahasiswa.pcr.ac.id>
2026-03-31 10:00:27 +0700

    Pertemuan3

--- src/pertemuan-2/HelloWorld.jsx
+++ src/pertemuan-2/HelloWorld.jsx
@@ -1,101 +1,38 @@
 export default function HelloWorld(){
-
-    const propsUserCard = {
-
-        nama: "Goku",
-
-        nim: "999999",
-
-        tanggal: "2025-01-01"
-
-    }
-
     return (
-
-        <>
-          <div className="logo-container">
-                <img src="img/logo-mjpht.png" alt="logo" className="logo-majapahit"/>
-            </div>
+        <div>
+            <img src="img/logo-mjpht.png" alt="logo" width="100%"/>
             <h1>Hello World</h1>
-
             <p>Selamat Belajar ReactJs</p>
-
-            <GreetingBinjai/>
-
-            <QuoteText/>
-
-            <UserCard
-
-                nama="Fikri"
-
-                nim="169412"
-
-                tanggal={new Date().toLocaleDateString()}
-
-              />
-
-              <UserCard {...propsUserCard}/>
-
-        </>
-
+          
+            <UserCard 
+	            nama="Fikri" 
+	            nim="169412"
+	            tanggal={new Date().toLocaleDateString()}
+	          />
+        </div>
     )
-
 }
-
-
-
-function GreetingBinjai(){
-
-    return(
-
-        <small>Salam dari Binjai 👌👋</small>
-
-    )
-
-}
-
-
 
 function QuoteText() {
-
     const text = "Mulutmu Harimaumu";
-
     const text2 = "Aku ingin jadi macan";
-
     return (
-
         <div>
-
             <hr/>
-
             <p>{text.toLowerCase()}</p>
-
             <p>{text2.toUpperCase()}</p>
-
         </div>
-
     )
-
 }
 
-
-
 function UserCard(props){
-
     return (
-
-        <>
-
+        <div>
             <hr/>
-
             <h3>Nama: {props.nama}</h3>
-
             <p>NIM: {props.nim}</p>
-
             <p>Tanggal: {props.tanggal}</p>
-
-        </>
-
+        </div>
     )
-
 }
