49b3c2352a999a99a09219b8d4cdc58ba071615e
aidil24si <aidil24si@mahasiswa.pcr.ac.id>
2026-04-07 10:26:43 +0700

    Pertemuan4Finish

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,12 +1,14 @@
 import { createRoot } from "react-dom/client";
 import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
 import './tailwind.css';
+import ResponsiveGrid from "./ResponsiveGrid";
 
 
 createRoot(document.getElementById("root"))
     .render(
         <div>           
-            <FrameworkListSearchFilter/>           
+            {/* <FrameworkListSearchFilter/>            */}
+            <ResponsiveGrid/>
         </div>
     )
 
